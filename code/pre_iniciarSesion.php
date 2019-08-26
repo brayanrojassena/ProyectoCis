b0f9060a0bc7087ec80a52fa08b1ef1398634d0c
Luis Carlos Hernández Peña <lucashernandez067@gmail.com>
2019-08-26 17:53:35 -0500

    pre_iniciarSesion.php

--- code/pre_iniciarSesion.php
+++ code/pre_iniciarSesion.php
@@ -3,23 +3,38 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<title>INICIAR SESION</title>
 </head>
 <body>
 	<div class="banner">
 		<?php include("est_banner.php"); ?>
 	</div>
-	<div class="sesion">
-		<h1>Iniciar Sesión</h1>
-		<form action="neg_evaluarsesion.php" autocomplete="off" method="post">
-			<label>Documento</label>
-			<input type="text" name="documento" placeholder="Número de Documento" pattern="[0-9 ]+" + required>
-			<label>Contraseña</label>
-			<input type="password" name="password" placeholder="Contraseña" + required>
-			</br>
-			<input type="submit" name="boton" value="Iniciar Sesión">
-		</form>
-		<center><h4>¿No tienes cuenta? <a href="pre_formregistro.php">Registrate</a></h4></center>
+	<div class="sesion container-fluid px-4 mt-3" width="100%">
+		<div class="row">
+			<div class="col">
+			</div>
+			<div class="col-sm-4 mx-auto bg-light border">
+				<h4 class="mt-2 py-4 text-center">Iniciar Sesión</h4>
+				<form action="neg_evaluarsesion.php" autocomplete="off" method="post">
+					<div class="form-group px-5">
+						<label>Documento</label>
+						<input type="text" class="form-control" name="documento" placeholder="Número de Documento" pattern="[0-9 ]+" + required>
+					</div>
+					<div class="form-group px-5">
+						<label>Contraseña</label>
+						<input type="password" class="form-control" name="password" placeholder="Contraseña" + required>
+					</div>
+					<div class="m-4 text-center">
+						<input type="submit" class="btn btn-primary" name="boton" value="Iniciar Sesión">
+					</div>
+				</form>
+				<p class="p-4">¿No tienes cuenta? <a href="pre_formregistro.php">Registrate</a></p>
+			</div>
+			<div class="col">
+
+			</div>
+		</div>
 	</div>
 	<div class="footer">
 		<?php include("est_footer.php"); ?>
