0337aeda087ae9bc984900c9a7915a8284e2805c
Luis Carlos Hernández Peña <lucashernandez067@gmail.com>
2019-09-09 15:52:24 -0500

    pre_iniciarSesionl.php Versión 1.0
    Imagen por defecto en la cuenta

--- code/pre_iniciarSesion.php
+++ code/pre_iniciarSesion.php
@@ -1,43 +1,45 @@
 <!DOCTYPE html>
-<html lang="es" dir="ltr">
+<html lang="es">
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<title>INICIAR SESION</title>
+	<title>Iniciar Sesión</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/bootstrap-material-design.min.css">
 </head>
 <body>
-	<div class="banner">
-		<?php include("est_banner.php"); ?>
-	</div>
-	<div class="sesion container-fluid px-4 mt-3" width="100%">
-		<div class="row">
-			<div class="col">
+	<div class="full-box login-container cover p-3">
+		<form action="" method="" autocomplete="off" class="logInForm">
+            <h2 class="text-center text-uppercase">Iniciar Sesión</h2>
+            <br>
+			<div class="form-group label-floating">
+			  <label class="control-label" for="UserDoc" style="color:#ffffff;">Documento</label>
+			  <input class="form-control" style="color:#ffffff;" id="UserDoc" name="documento" type="text">
+			  <p class="help-block">Escribe tu número de identificación</p>
 			</div>
-			<div class="col-sm-4 mx-auto bg-light border">
-				<h4 class="mt-2 py-4 text-center">Iniciar Sesión</h4>
-				<form action="neg_evaluarsesion.php" autocomplete="off" method="post">
-					<div class="form-group px-5">
-						<label>Documento</label>
-						<input type="text" class="form-control" name="documento" placeholder="Número de Documento" pattern="[0-9 ]+" + required>
-					</div>
-					<div class="form-group px-5">
-						<label>Contraseña</label>
-						<input type="password" class="form-control" name="password" placeholder="Contraseña" + required>
-					</div>
-					<div class="m-4 text-center">
-						<input type="submit" class="btn btn-primary" name="boton" value="Iniciar Sesión">
-					</div>
-				</form>
-				<p class="p-4">¿No tienes cuenta? <a href="pre_formregistro.php">Registrate</a></p>
+			<div class="form-group label-floating">
+			  <label class="control-label" for="UserPass" style="color:#ffffff;">Contraseña</label>
+			  <input class="form-control" style="color:#ffffff;" id="UserPass" name="password" type="password">
+			  <p class="help-block">Escribe tu contraseña</p>
 			</div>
-			<div class="col">
-
+			<div class="form-group text-center">
+				<input type="submit" value="Iniciar sesión" class="btn btn-info" style="color: #FFF;">
 			</div>
-		</div>
-	</div>
-	<div class="footer">
-		<?php include("est_footer.php"); ?>
-	</div>
+			<div class="">
+            	<p>¿No tienes cuenta? <a href="">Regístrate</a></p>   
+        	</div>
+        </form>
+    </div>
+	<!--====== Scripts -->
+	<script src="./js/jquery-3.1.1.min.js"></script>
+	<script src="./js/bootstrap.min.js"></script>
+	<script src="./js/material.min.js"></script>
+	<script src="./js/ripples.min.js"></script>
+	<script src="./js/sweetalert2.min.js"></script>
+	<script src="./js/jquery.mCustomScrollbar.concat.min.js"></script>
+	<script src="./js/main.js"></script>
+	<script>
+		$.material.init();
+	</script>
 </body>
 </html>
