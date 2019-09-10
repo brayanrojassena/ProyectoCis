4b7f9997e568f6851384919aa524c0226d63ff9a
Luis Carlos Hernández Peña <lucashernandez067@gmail.com>
2019-09-10 13:08:10 -0500

    neg_conexion.php versión 1.1
    Capa de negocio de la conexión con la base de datos. (para el servidor)
    
    Co-Authored-By: juangambaaprendiz <juangambaaprendiz@users.noreply.github.com>

--- code/neg_conexion.php
+++ code/neg_conexion.php
@@ -1,5 +1,5 @@
 <?php 
-	$db = new MySQLi('localhost', 'root', '', 'amoblando');
+	$db = new MySQLi('sql104.epizy.com', 'epiz_24239209', 'd0rV99NmMfA80V', 'epiz_24239209_db_ngr_inventario');
 	$acentos = $db->query("SET NAMES 'utf8'");
 	if($db->connect_error > 0){
 		die('Error en la conexión [' .$db->connect_error. ']');
